cf369e2d3baf40187afa331f5e1cfdc2eb9a48e8
KevinAlvarez7 <33540851+KevinAlvarez7@users.noreply.github.com>
2020-09-01 06:57:53 +0800

    Updates

---
+++ precache-manifest.9b1f5bde04dc3da8ffa83b1bed4b7a0a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2844c5063b00bc9308361cc9556468a9",
+    "url": "/zeros-landing/index.html"
+  },
+  {
+    "revision": "d24c7668d278c148270d",
+    "url": "/zeros-landing/static/css/main.e174cf80.chunk.css"
+  },
+  {
+    "revision": "cfdd76304f1c36e9a7b6",
+    "url": "/zeros-landing/static/js/2.52338932.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "/zeros-landing/static/js/2.52338932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d24c7668d278c148270d",
+    "url": "/zeros-landing/static/js/main.3aa02138.chunk.js"
+  },
+  {
+    "revision": "83f50c39b487ea9027fa",
+    "url": "/zeros-landing/static/js/runtime-main.0592816b.js"
+  },
+  {
+    "revision": "cf2ea7435d9f6c3c873da2fa2e27e507",
+    "url": "/zeros-landing/static/media/banner-img-nonClay.cf2ea743.png"
+  },
+  {
+    "revision": "a28d57198440cd2b6b7a7a6e6c9665cf",
+    "url": "/zeros-landing/static/media/play.a28d5719.svg"
+  }
+]);
